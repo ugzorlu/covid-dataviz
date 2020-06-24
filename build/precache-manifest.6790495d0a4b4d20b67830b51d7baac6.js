74c35184f27c8485f130546e0d8d102914fe77a7
Umut Gencay Zorlu <umutgencayzorlu@gmail.com>
2020-06-24 21:01:34 +0300

    Add files via upload

---
+++ build/precache-manifest.6790495d0a4b4d20b67830b51d7baac6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e5c132d68078da6df83fa97b0da299d",
+    "url": "/ugzorlu/covid-dataviz/index.html"
+  },
+  {
+    "revision": "7b9937954dc1b8dff14d",
+    "url": "/ugzorlu/covid-dataviz/static/css/main.13e2374c.chunk.css"
+  },
+  {
+    "revision": "954a2673268d63451cce",
+    "url": "/ugzorlu/covid-dataviz/static/js/2.cec4af7c.chunk.js"
+  },
+  {
+    "revision": "1f343f3b00bd42841cd23cdb6e2b08ec",
+    "url": "/ugzorlu/covid-dataviz/static/js/2.cec4af7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b9937954dc1b8dff14d",
+    "url": "/ugzorlu/covid-dataviz/static/js/main.63034156.chunk.js"
+  },
+  {
+    "revision": "f7ba0ed07ecdcea75d32",
+    "url": "/ugzorlu/covid-dataviz/static/js/runtime-main.1cdd1ca0.js"
+  }
+]);
